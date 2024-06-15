1027d160c9d8d2b3e4541dd63d3c9d935f4ae74b
Lanthoiba2022 <khumanthemlanthoiba@gmail.com>
2024-06-15 11:29:48 +0530

    backend updated with relevant files

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const rootRouter = require('./routes/index');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.use('/api/vi',rootRouter); //mainRouter == rootRouter
+
+console.log("App listening on port 3000\n");
+app.listen(3000);
