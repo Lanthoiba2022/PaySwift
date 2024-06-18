eb0e3598148df6cb371d2018bd37b8046189b37d
Arvind Batham <arvindbatham600@gmail.com>
2024-06-18 22:22:47 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const rootRouter = require('./routes/index');
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/v1',rootRouter); //mainRouter == rootRouter
-
-// console.log("App listening on port 3000\n");
-// app.listen(3000);
-
-
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
