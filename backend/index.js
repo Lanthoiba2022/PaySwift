be978aec8e22c4255cadbb2bc1b0077df7b4f2d7
Lanthoiba2022 <khumanthemlanthoiba@gmail.com>
2024-06-18 21:51:41 +0530

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse incoming JSON requests
 
 // MongoDB connection
-const mongoURI = "mongodb+srv://khumanthemlanthoiba:yjzP9B4x57cp7X9W@cluster0.tvhtm8q.mongodb.net/test"; 
+const mongoURI = "mongodb+srv://khumanthemlanthoiba:yjzP9B4x57cp7X9W@cluster0.tvhtm8q.mongodb.net/PaySwift?retryWrites=true&w=majority"; 
 mongoose.connect(mongoURI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
