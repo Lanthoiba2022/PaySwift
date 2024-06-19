674d76dcf5703467af55aa74862147b6c7002492
Lanthoiba2022 <khumanthemlanthoiba@gmail.com>
2024-06-19 06:50:32 +0530

    changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -16,16 +16,16 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const rootRouter = require('./routes/index');
-
+require('dotenv').config
 const app = express();
 
 // Middleware setup
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); // Enable CORS for all routes, add allow specific origins here frontend link
 app.use(express.json()); // Parse incoming JSON requests
 
 // MongoDB connection
-const mongoURI = "mongodb+srv://khumanthemlanthoiba:yjzP9B4x57cp7X9W@cluster0.tvhtm8q.mongodb.net/PaySwift?retryWrites=true&w=majority"; 
-mongoose.connect(mongoURI);
+
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
